621f1fddb45709d648ee0a4b81927a45d6df368f
Jonathan Miller <jonathan.michael.miller@gmail.com>
2013-12-14 22:33:24 -0500

    initial commit

---
+++ dummy-server.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+var util = require('util');
+var os   = require('os');
+var http = require('http');
+var count = 0;
+
+/*var bulkhead = [
+  "                  _       ",
+  "                 | |      ",
+  "  _ __   ___   __| | ___  ",
+  " | '_ \\ / _ \\ / _` |/ _ \\ ",
+  " | | | | (_) | (_| |  __/ ",
+  " |_| |_|\\___/ \\__,_|\\___| ",
+  "                          ",
+  "                          "
+];*/
+
+var bulkhead = [
+  "  _   _           _        _      ",
+  " | \\ | |         | |      (_)     ",
+  " |  \\| | ___   __| | ___   _ ___  ",
+  " | . ` |/ _ \\ / _` |/ _ \\ | / __| ",
+  " | |\\  | (_) | (_| |  __/_| \\__ \\ ",
+  " \\_| \\_/\\___/ \\__,_|\\___(_) |___/ ",
+  "                         _/ |     ",
+  "                        |__/      "
+];
+
+http.createServer(function (req, res) {
+  console.log( 'incoming %d', count );
+  count++;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  //Linux blue 3.12.5-gentoo #1 SMP Fri Dec 13 11:31:33 EST 2013 x86_64 AMD Phenom(tm) II X6 1090T Processor AuthenticAMD GNU/Linux
+
+  var uname = util.format( '%s %s %s %s %s %s', 
+  	os.type(),
+  	os.hostname(), 
+  	process.version,
+  	new Date(),
+  	process.arch,
+  	os.cpus()[0].model );
+
+
+  var out = [
+  	'Hello World',
+  	'Path->'+req.url,
+    'NodeJS ' + process.version,
+    uname,
+    'headers->'
+  ];
+  Object.keys(req.headers).sort().forEach( function( it ) {
+    out.push(it+': '+req.headers[it]);
+  });
+  res.end([].concat( bulkhead, out ).join('\n'));
+}).listen(1337);
+console.log('Server running at http://127.0.0.1:1337/');
+
